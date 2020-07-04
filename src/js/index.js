849437c7e3bec48df2cc64c76aae47da688429da
Nigmatulin8 <rustam280897@gmail.com>
2020-07-04 22:34:00 +0400

    v2.2.1

---
+++ src/js/index.js
@@ -0,0 +1,34 @@
+let slides = document.querySelectorAll("img[alt=slide]");
+
+let btnRight = document.querySelector(".btnRight");
+let btnLeft = document.querySelector(".btnLeft");
+
+let i = 0;
+
+btnRight.addEventListener("click", () => {
+    ++i;
+
+    if (i >= slides.length) {
+        slides[i-1].classList.remove("block");
+        i = 0;
+        slides[i].classList.add("block");
+    } 
+    else {
+        slides[i-1].classList.remove("block");
+        slides[i].classList.add("block");
+    }
+});
+
+btnLeft.addEventListener("click", () => {
+    --i;
+
+    if (i < 0) {
+        slides[i+1].classList.remove("block");
+        i = slides.length - 1;
+        slides[i].classList.add("block");
+    } 
+    else {
+        slides[i+1].classList.remove("block");
+        slides[i].classList.add("block");
+    }
+})
